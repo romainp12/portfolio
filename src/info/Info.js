fa10e8615950f324d5ce29c8136265abfe67c996
Romain <53324953+romainp12@users.noreply.github.com>
2025-01-24 14:13:36 +0100

    Update Info.js

--- src/info/Info.js
+++ src/info/Info.js
@@ -1,36 +1,19 @@
 import self from "../img/self.png"
 import mock1 from "../img/load2.gif"
 
-/* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
-how to make this template work best for you! */
-
 export let colors = ["rgb(0,255,164)", "rgb(166,104,255)"];
-/*
-I highly recommend using a gradient generator like https://gradientgenerator.paytonpierce.dev/ to generate a pair of colors that you like.
-These colors will be used to style your name on the homepage, the background of your picture, and some other accents throughout
-the site.
- */
 
-/* 
-This variable will change the layout of the website from multipage to single, scrollable page
-*/
 export let singlePage = false;
-
-/*
-So let's get started! Some of the info below is pretty self-explanatory, like 'firstName' and 'bio'. I'll try to explain anything
-that might not be obvious right off the bat :) I recommend looking at the template example live using "npm start" to get an idea
-of what each of the values mean.
- */
 
 export const info = {
     firstName: "Romain",
     lastName: "Pereira",
-    initials: "RP", // the example uses first and last, but feel free to use three or more if you like.
+    initials: "RP",
     position: "Full Stack Student Developer",
-    selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
-    gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
+    selfPortrait: self,
+    gradient: `-webkit-linear-gradient(135deg, ${colors})`,
     baseColor: colors[0],
-    miniBio: [ // these are just some "tidbits" about yourself. You can look at mine https://paytonjewell.github.io/#/ for an example if you'd like
+    miniBio: [
         {
             emoji: '🇫🇷',
             text: 'Based in Paris, France'
@@ -74,29 +57,28 @@ export const info = {
     ,
     hobbies: [
         {
-            label: 'reading',
-            emoji: '📖'
+            label: 'Traveling',
+            emoji: '✈️'
         },
         {
-            label: 'theater',
-            emoji: '🎭'
+            label: 'Video Games',
+            emoji: '🎮'
         },
         {
-            label: 'movies',
-            emoji: '🎥'
+            label: 'Soccer',
+            emoji: '⚽️'
         },
         {
-            label: 'cooking',
-            emoji: '🌶'
+            label: 'Padel',
+            emoji: '🎾'
         }
-// Same as above, change the emojis to match / relate to your hobbies or interests.
-// You can also remove the emojis if you'd like, I just think they look cute :P
+        
     ],
-    portfolio: [ // This is where your portfolio projects will be detailed
+    portfolio: [
         {
             title: "Coming soon 🔜",
-            live: "#", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
-            source: "#", // this should be a link to the **repository** of the project, where the code is hosted.
+            live: "#",
+            source: "#",
             image: mock1
         }
     ]
