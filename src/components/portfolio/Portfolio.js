ce0cdef9146fda4864fdbf0c27f626fedf6a6368
Romain <53324953+romainp12@users.noreply.github.com>
2025-03-11 18:21:05 +0100

    Update Portfolio.js

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -2,26 +2,9 @@ import React from 'react';
 import PortfolioBlock from "./PortfolioBlock";
 import {Box, Grid} from "@mui/material";
 import {info} from "../../info/Info";
-import Terminal from "../about/Terminal";
 import Style from "../about/About.module.scss";
 
 export default function Portfolio() {
-
-    const firstName = info.firstName.toLowerCase()
-
-    function projectsText() {
-        return <>
-            <p><span style={{color: info.baseColor}}>{firstName}{info.lastName.toLowerCase()} $</span> cd
-                projects</p>
-            <p><span style={{color: info.baseColor}}>projects <span
-                className={Style.green}>(main)</span> $</span> ls</p>
-            <ul>
-                {info.projects.map((project, index) => (
-                    <li key={index}><Box component={'span'} mr={'1rem'}>{project.name}</Box></li>
-                ))}
-            </ul>
-        </>;
-    }
 
     return (
         <Box>
