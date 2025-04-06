12240cd8f45a2fb986d4558ea05d005f22e42105
Romain <53324953+romainp12@users.noreply.github.com>
2025-04-07 01:47:22 +0200

    Update BaseLayout.js

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -45,7 +45,7 @@ export default function BaseLayout() {
                <Navbar darkMode={darkMode} handleClick={handleToggleDarkMode} active={active} setActive={setActive} />
             </Grid>
             <Grid item flexGrow={1}>
-               {singlePage ? <SinglePageRoutes refs={{refHome, refAbout, refPortfolio}}/> : <MultiPageRoutes />}
+               {singlePage ? <SinglePageRoutes refs={{refHome, refAbout, refPortfolio, refTechwatch}}/> : <MultiPageRoutes />}
             </Grid>
             <Grid item>
                <Box component={'footer'} display={'flex'} flexDirection={'column'} alignItems={'center'}
