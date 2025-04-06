bb42a2bdb80c86262308bb29f9df4496cb94827c
Romain <53324953+romainp12@users.noreply.github.com>
2025-04-07 01:43:03 +0200

    Create Techwatch.js

---
+++ src/components/techwatch/Techwatch.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import TechwatchBlock from "./TechwatchBlock";
+import {Box, Grid} from "@mui/material";
+import {info} from "../../info/Info";
+ 
+ export default function Techwatch() {
+   return (
+     <Box>
+       <Grid container display={'flex'} justifyContent={'center'}>
+         {info.portfolio.map((project, index) => (
+           <Grid item xs={12} md={6} key={index}>
+             <PortfolioBlock
+               image={project.image}
+               live={project.live}
+               source={project.source}
+               title={project.title}
+               description={project.description}
+             />
+           </Grid>
+         ))}
+       </Grid>
+     </Box>
+   );
+ }
